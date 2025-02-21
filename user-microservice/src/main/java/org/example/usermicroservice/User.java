f42ad241d09e16a45ca445dea0458ee149ee46ce
Gleb <gleb.margolin.1301@gmail.com>
2025-02-21 11:50:38 +0300

    исправлена ошибка с созданием пользователя

--- user-microservice/src/main/java/org/example/usermicroservice/User.java
+++ user-microservice/src/main/java/org/example/usermicroservice/User.java
@@ -18,7 +18,7 @@ import java.util.Calendar;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String login;
