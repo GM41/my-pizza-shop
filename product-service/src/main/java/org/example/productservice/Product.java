9076f363256debe26862cb894d9af18b5f3a5bce
Gleb <gleb.margolin.1301@gmail.com>
2025-02-25 19:31:21 +0300

    added entity and dto classes, and also repository

---
+++ product-service/src/main/java/org/example/productservice/Product.java
@@ -0,0 +1,22 @@
+package org.example.productservice;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="products")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int price;
+}
---
+++ product-service/src/main/java/org/example/productservice/ProductDto.java
@@ -0,0 +1,13 @@
+package org.example.productservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private String name;
+    private int price;
+}
