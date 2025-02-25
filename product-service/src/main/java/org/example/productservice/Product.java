c6d2ececa5139e6958f2a39af1dd74f9f8a445ce
Gleb <gleb.margolin.1301@gmail.com>
2025-02-25 21:11:33 +0300

    categoryId renamed to category_id

--- product-service/src/main/java/org/example/productservice/Product.java
+++ product-service/src/main/java/org/example/productservice/Product.java
@@ -4,12 +4,10 @@ package org.example.productservice;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="products")
-@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,5 +17,37 @@ public class Product {
     private Long id;
     private String name;
     private int price;
-    private Long categoryId;
+    private Long category_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long categoryId) {
+        this.category_id = categoryId;
+    }
 }
--- product-service/src/main/java/org/example/productservice/ProductDto.java
+++ product-service/src/main/java/org/example/productservice/ProductDto.java
@@ -1,14 +1,36 @@
 package org.example.productservice;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDto {
     private String name;
     private int price;
-    private Long categoryId;
+    private Long category_id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
 }
